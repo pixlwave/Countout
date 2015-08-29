ebb86a6d4dd691bef1334385a13ded66d7c3b44c
Doug <git@digitalfx.me.uk>
2015-08-29 16:12:02 +0100

    Add spaces to user defaults keys to avoid collision with data set with RubyMotion

--- Countout/Appearance.swift
+++ Countout/Appearance.swift
@@ -3,22 +3,22 @@ import UIKit
 class Appearance {
     static let sharedClient = Appearance()
     
-    var backgroundColor: UIColor { didSet { Defaults["BackgroundColor"] = NSKeyedArchiver.archivedDataWithRootObject(backgroundColor) } }
-    var backgroundImage: UIImage? { didSet { Defaults["BackgroundImage"] = UIImagePNGRepresentation(backgroundImage) } }
-    var fontFamily: String { didSet { Defaults["FontFamily"] = fontFamily } }
-    var fontScale: CGFloat { didSet { Defaults["FontScale"] = fontScale } }
-    var fontWeight: String { didSet { Defaults["FontWeight"] = fontWeight } }
-    var textColor: UIColor { didSet { Defaults["TextColor"] = NSKeyedArchiver.archivedDataWithRootObject(textColor) } }
+    var backgroundColor: UIColor { didSet { Defaults["Background Color"] = NSKeyedArchiver.archivedDataWithRootObject(backgroundColor) } }
+    var backgroundImage: UIImage? { didSet { Defaults["Background Image"] = UIImagePNGRepresentation(backgroundImage) } }
+    var fontFamily: String { didSet { Defaults["Font Family"] = fontFamily } }
+    var fontScale: CGFloat { didSet { Defaults["Font Scale"] = fontScale } }
+    var fontWeight: String { didSet { Defaults["Font Weight"] = fontWeight } }
+    var textColor: UIColor { didSet { Defaults["Text Color"] = NSKeyedArchiver.archivedDataWithRootObject(textColor) } }
     
     let defaults = NSUserDefaults.standardUserDefaults()
     
     init() {
-        backgroundColor = Defaults["BackgroundColor"].color ?? UIColor(red: 0.0, green: 0.0, blue: 72.0/255.0, alpha: 1.0)
-        backgroundImage = Defaults["BackgroundImage"].image
-        fontFamily = Defaults["FontFamily"].string ?? "AvenirNext"
-        fontScale = Defaults["FontScale"].cgfloat ?? 0.25
-        fontWeight = Defaults["FontWeight"].string ?? "UltraLight"
-        textColor = Defaults["TextColor"].color ?? UIColor(red: 1.0, green:1.0, blue:1.0, alpha:1.0)
+        backgroundColor = Defaults["Background Color"].color ?? UIColor(red: 0.0, green: 0.0, blue: 72.0/255.0, alpha: 1.0)
+        backgroundImage = Defaults["Background Image"].image
+        fontFamily = Defaults["Font Family"].string ?? "AvenirNext"
+        fontScale = Defaults["Font Scale"].cgfloat ?? 0.25
+        fontWeight = Defaults["Font Weight"].string ?? "UltraLight"
+        textColor = Defaults["Text Color"].color ?? UIColor(red: 1.0, green:1.0, blue:1.0, alpha:1.0)
     }
     
     func reset() {
