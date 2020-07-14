74405ada97c88d45b0d04fa1cf62e58a2684b959
Doug <git@pixlwave.uk>
2020-07-14 13:30:17 +0100

    Add ExternalSceneDelegate. Add external display configuration to Info.plist. Remove broken external display code from AppDelegate.

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -1,48 +1,10 @@
 import UIKit
-import SwiftUI
 
-//@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-    var countdownVC: CountdownController?
     
-    var externalWindow: UIWindow?
-    var outputVC: UIHostingController<CountdownView>?
-    
-    let countdown = CountdownTimer.shared
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         application.isIdleTimerDisabled = true
-        
-        countdownVC = window?.rootViewController as? CountdownController
-        
-        updateOutput()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(updateOutput), name: UIScreen.didConnectNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(updateOutput), name: UIScreen.didDisconnectNotification, object: nil)
-        
         return true
-    }
-    
-    @objc func updateOutput() {
-        if UIScreen.screens.count > 1 {
-            externalWindow = UIWindow(frame: UIScreen.screens[1].bounds)
-            externalWindow?.screen = UIScreen.screens[1]
-            
-            outputVC = UIHostingController<CountdownView>(rootView: CountdownView())
-            
-            externalWindow?.rootViewController = outputVC
-            externalWindow?.isHidden = false
-            
-            // countdownVC?.outputExists = true
-        } else {
-            countdownVC?.outputVC = nil
-            outputVC = nil
-            externalWindow = nil
-            
-            // countdownVC?.outputExists = false
-        }
     }
     
 }
---
+++ Sources/ExternalSceneDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import SwiftUI
+
+class ExternalSceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene, session.role == .windowExternalDisplay else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        let hostingController = UIHostingController<CountdownView>(rootView: CountdownView())
+        window?.rootViewController = hostingController
+        window?.isHidden = false
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        window = nil
+    }
+}
